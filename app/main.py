08b7ffad6f7c3c9eccf66f249875ba28f687cf17
faaiz99 <44613754+faaiz99@users.noreply.github.com>
2023-12-04 01:31:31 +0500

    main.py reverted

--- app/main.py
+++ app/main.py
@@ -8,9 +8,8 @@ import cv2
 from keras.models import model_from_json
 from keras.preprocessing import image
 import keras.utils as image
-import asyncio
-
 model = model_from_json(open("app/fer.json", "r").read())
+#load weights
 model.load_weights('app/fer.h5')
 face_haar_cascade = cv2.CascadeClassifier('app/haarcascade_frontalface_default.xml')
 
@@ -28,43 +27,49 @@ app.add_middleware(
     allow_headers = headers    
 )
 
+
+
+
+
 @app.post("/process_image/")
 async def process_image(files: List[UploadFile] = File(...)):
     print(files)
     if not files:
         raise HTTPException(status_code=400, detail="No file provided")
 
-    tasks = [process_file(file) for file in files]
-    return await asyncio.gather(*tasks)
+    # Process each uploaded file
+    results = []
+    for file in files:
+        try:
+            contents = await file.read()
+            test_img = cv2.imdecode(np.frombuffer(contents, np.uint8), cv2.COLOR_BGR2GRAY)
+            gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
 
-async def process_file(file: UploadFile):
-    contents = b''
-    async for chunk in file.iter_any(1024 * 1024):  # 1MB at a time
-        contents += chunk
-    test_img = cv2.imdecode(np.frombuffer(contents, np.uint8), cv2.COLOR_BGR2GRAY)
-    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+            faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
 
-    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+            for (x,y,w,h) in faces_detected:
+                cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=3)
+                roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from  image
+                roi_gray=cv2.resize(roi_gray,(48,48))
+                img_pixels = image.img_to_array(roi_gray)
+                img_pixels = np.expand_dims(img_pixels, axis = 0)
+                
+                img_pixels = img_pixels.astype('float64') / 255 #normalizing
 
-    for (x,y,w,h) in faces_detected:
-        cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=3)
-        roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from  image
-        roi_gray=cv2.resize(roi_gray,(48,48))
-        img_pixels = image.img_to_array(roi_gray)
-        img_pixels = np.expand_dims(img_pixels, axis = 0)
-        
-        img_pixels = img_pixels.astype('float64') / 255 #normalizing
+                predictions = model.predict(img_pixels)
 
-        predictions = model.predict(img_pixels)
+                #find max indexed array
+                max_index = np.argmax(predictions[0])
 
-        #find max indexed array
-        max_index = np.argmax(predictions[0])
+                emotions = ('angry', 'disgust', 'Anxiety', 'happy', 'Depressed', 'surprise', 'neutral')
+                predicted_emotion = emotions[max_index]
 
-        emotions = ('angry', 'disgust', 'Anxiety', 'happy', 'Depressed', 'surprise', 'neutral')
-        predicted_emotion = emotions[max_index]
-
-    return predicted_emotion
-
+            return predicted_emotion
+        except Exception as e:
+            results.append(str(e))
+            
+    return results
+    
 @app.get('/test')
 def here():
     return {"test": "workings"}
